1c3b2446a1a7f145326ae1372c1f9a9c1ff4a841
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-09-30 20:05:53 +0000

    Solve

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,31 +1,35 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	//Question 1
+    public static int sum2D(int[][] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                sum += numbers[i][j];
+            }
+        }
+        return sum;
+    }
 
-	}
+	//Question 2 
+	public static int sumRow(int[][] numbers, int row) {
+    int sum = 0;
+        for (int j = 0; j < numbers[row].length; j++) {
+            sum += numbers[row][j];
+        }
+        return sum;
+    }
 
-	public static void q1() {
-		//Write question 1 code here
-		int[][] numbers = {
-			{1, 3, 5 },
-			{2, 4, 6, 7},
-			{3, 5, 6}
-		};
-		System.out.println(numbers[0][0] + numbers[0][1] + numbers[0][2]);
-		System.out.println(numbers[1][0] + numbers[1][1] + numbers[1][2] + numbers[1][3]); 
-		System.out.println(numbers[2][0] + numbers[2][1] + numbers[2][2]);
-	}
-
-	public static void q2() {
-		int[][] numbers = {
-			{},
-			{}
-		};
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
+	//Question 3
+ public static int sumColumn(int[][] numbers, int col) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (col < numbers[i].length) {
+                sum += numbers[i][col];
+            }
+        }
+        return sum;
+    }
 
 	public static void q4() {
 		//Write question 4 code here
