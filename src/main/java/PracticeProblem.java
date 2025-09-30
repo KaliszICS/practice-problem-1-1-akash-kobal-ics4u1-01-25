d2867089b648d602d14c9b25aeb2f344ee69535b
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-09-30 18:46:08 +0000

    test

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,10 +6,21 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		int[][] numbers = {
+			{1, 3, 5 },
+			{2, 4, 6, 7},
+			{3, 5, 6}
+		};
+		System.out.println(numbers[0][0] + numbers[0][1] + numbers[0][2]);
+		System.out.println(numbers[1][0] + numbers[1][1] + numbers[1][2] + numbers[1][3]); 
+		System.out.println(numbers[2][0] + numbers[2][1] + numbers[2][2]);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int[][] numbers = {
+			{},
+			{}
+		};
 	}
 
 	public static void q3() {
